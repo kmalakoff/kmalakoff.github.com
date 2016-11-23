6281c6b3ee3868b34359d21f53178e34be82d290
Kevin Malakoff <kmalakoff@gmail.com>
2016-11-23 13:07:52 -0800

    Clean up

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,9 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.js?$/, exclude: /(node_modules)/, loader: 'babel'},
-      {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')},
-      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff"},
-      {test: /\.(ttf|eot|svg|jpg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"}
+      {test: /\.js?$/, loader: 'babel'},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css-loader')},
+      {test: /\.jpg$/, loader: 'file-loader'}
     ]
   },
 
