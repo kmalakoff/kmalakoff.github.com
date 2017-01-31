28932756f48ad7090fe1319fcc7239d57d36378d
Kevin Malakoff <kmalakoff@gmail.com>
2017-01-31 11:49:55 -0800

    Release

--- client/index.js
+++
@@ -1,7 +0,0 @@
-import './css/style.css';
-
-import React from 'react';
-import {render} from 'react-dom';
-import PageLanding from './components/page-landing';
-
-render(<PageLanding/>, document.getElementById('app'));
